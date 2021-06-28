56cddee4d1b5d7587e151efb878c75c75ebe60b7
Rohan MM <rohanmiraje19@gmail.com>
2021-06-28 10:43:25 +0530

    -day11: practise problems/concepts from hackerrank --> 30 days of code - 2D array pattern traversing

---
+++ DSA/pythonhackerrank/2d_pattern_traversal.py
@@ -0,0 +1,97 @@
+"""
+Objective
+Today, we are building on our knowledge of arrays by adding another dimension. Check out the Tutorial tab for learning materials and an instructional video.
+
+Context
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is  for the hourglass in the top left corner.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers that describe the 2D Array .
+
+Constraints
+
+Output Format
+
+Print the maximum hourglass sum in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+"""
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = 0
+    # sample of firts first pattern
+    # sum(arr[0][0:3]) + arr[1][1] + sum(arr[2][0:3])
+    result = []
+    for i in range(4):
+        for j in range(4):
+            curr_sum = sum(arr[i][j:j + 3]) + arr[i + 1][j + 1] + sum(arr[i + 2][j:j + 3])
+            result.append(curr_sum)
+    print(max(result))
