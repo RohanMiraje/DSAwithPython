5f3253b8358cb215c75d782e95be1aea896283ec
RohanMiraje <rohanmiraje19@gmail.com>
2021-09-13 15:52:11 +0530

    update reformat

--- DSA/maths/debt_cal.py
+++ DSA/maths/debt_cal.py
@@ -1,30 +1,14 @@
-principal = 400000
-roi = 1.5
-
-
-def get_interest():
-    net_int = 0
-    outstanding = principal
-    fix_contribution = 15000
-    for i in range(36):
-        if outstanding > 0:
-            paid_int = outstanding * (roi / 100)
-            contri = fix_contribution
-            contri -= paid_int
-            outstanding -= contri
-            net_int += paid_int
-    return net_int, outstanding
-
-
 def emi_calculator(loan_amount, no_of_years, roi):
-    # [P.r.(1+r)^n/ (1+r)^n-1]
+    # emi formula [P.r.(1+r)^n/ (1+r)^n-1]
     p = loan_amount
-    n = no_of_years
-    r = roi
-    emi = (p * r) * (1 + r) ** n / ((1 + r) ** n) - 1
+    n = no_of_years * 12  # no of months
+    r = (roi / 12) / 100  # monthly basis interest
+    emi = ((p * r) * (1 + r) ** n) / (((1 + r) ** n) - 1)
+    interest = emi * n - p
+    total = emi * n
+    print(f"for {no_of_years=} {interest=} {total=}")
     return emi
 
 
 if __name__ == '__main__':
-    print(f"{get_interest()}")
-    print(f"{emi_calculator(150000, 2, 16.75)}")
+    print(f"{emi_calculator(1000000, 10, 10.5)=}")
