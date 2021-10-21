ea4884afcac9d6cc2817a93e918c829dd10cef5d
RohanMiraje <rohanmiraje19@gmail.com>
2021-10-21 06:36:06 +0530

    30 days of code: day 4 -anagram problem variant

--- DSA/string/anagrams.py
+++ DSA/string/anagrams.py
@@ -16,6 +16,32 @@ def are_anagrams(str1, str2):
     return res ^ res2 == 0
 
 
+my_list = ["abc", "acb", "pto", "cab", "bac", "pot", "top"]
+my_str = "abc"
+
+
+def check_anagram(val1, val2):
+    aux_dict = dict()
+    joined = val1.strip() + val2.strip()
+    # abcacb
+    for letter in joined:
+        if letter in aux_dict:
+            aux_dict[letter] += 1
+        else:
+            aux_dict[letter] = 1
+    for key, value in aux_dict.items():
+        if value != 2:
+            return False
+    return True
+
+
+# O(n*len(anagram)
+result = []
+for val in my_list:
+    if check_anagram(val, my_str) is True:
+        result.append(val)
+print(result)
+
 if __name__ == '__main__':
     s1 = "rohan mukund miraje"
     s2 = "miraje rohan mukund"
